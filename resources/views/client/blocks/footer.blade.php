f82ecf77558c22bb06145b11921d379566a9f900
nhatduynguyen <15520166@gm.uit.edu.vn>
2021-09-06 23:38:19 +0700

    Create footer.blade.php

---
+++ resources/views/client/blocks/footer.blade.php
@@ -0,0 +1,94 @@
+ <!-- Footer -->
+<footer class="bg-primary text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/UIT.Fanpage" target="_blank" role="button"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+      <!-- Twitter -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/uituniversity/" target="_blank" role="button"
+        ><i class="fab fa-instagram"></i
+      ></a>
+      <!-- Linkedin -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/school/university-of-information-technology/" target="_blank" role="button"
+        ><i class="fab fa-linkedin-in"></i
+      ></a>
+      <!-- Google -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.uit.edu.vn/" target="_blank" role="button"
+        ><i class="fab fa-google"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Form -->
+    <!-- <section class="">
+      <form action="">
+        
+        <div class="row d-flex justify-content-center">
+         
+          <div class="col-auto">
+            <p class="pt-2">
+              <strong>Sign up for our newsletter</strong>
+            </p>
+          </div> -->
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <!-- <div class="col-md-5 col-12"> -->
+            <!-- Email input -->
+            <!-- <div class="form-outline form-white mb-4">
+              <input type="email" id="form5Example2" class="form-control" />
+              <label class="form-label" for="form5Example2">Email address</label>
+            </div>
+          </div> -->
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <!-- <div class="col-auto"> -->
+            <!-- Submit button -->
+            <!-- <button type="submit" class="btn btn-outline-light mb-4">
+              Subscribe
+            </button>
+          </div> -->
+          <!--Grid column-->
+        <!-- </div> -->
+        <!--Grid row-->
+      </form>
+    </section>
+    <!-- Section: Form -->
+
+    <!-- Section: Text -->
+    <section class="mb-4">
+      <p>
+                Phòng A120, Trường Đại học Công nghệ Thông tin - 
+						Khu phố 6, P.Linh Trung, Q.Thủ Đức, TP.Hồ Chí Minh.
+      </p>
+    </section>
+    <!-- Section: Text -->
+	<div class="bg-primary text-center text-white">
+					<div class="widget widget-listings">
+						<div class="h2 text-white">Liên kết</div>
+						<ul>
+							<li><a class="text-white" href="{{ route('client.home') }}"><i class="text-white fas fa-angle-right pr-2"></i>Trang chủ</a></li>
+							<li><a class="text-white" href="https://daa.uit.edu.vn/"  target="_blank"><i class="text-white fas fa-angle-right pr-2"></i>Cổng thông tin đào tạo</a></li>
+							<li><a class="text-white" href="http://CTSV.UIT.EDU.VN/" target="_blank"><i class="text-white fas fa-angle-right pr-2"></i>Phòng công tác sinh viên</a></li>
+							<li><a class="text-white" href="http://oep.uit.edu.vn/" target="_blank"><i class="text-white fas fa-angle-right pr-2"  target="_blank"></i>Văn phòng Các chương trình đặc biệt</a></li>
+							<li><a class="text-white" href="http://forum.uit.edu.vn/" target="_blank"><i class="text-white fas fa-angle-right pr-2"  target="_blank"></i>Forum</a></li>
+						</ul>
+					</div>
+				</div>
+  <!-- Copyright -->
+  <div class="bg-primary text-center text-white p-3">
+    © 2021 Website:    
+    <a class="text-white" href="https://diemdanhuit.tech/">diemdanhuit.tech</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
