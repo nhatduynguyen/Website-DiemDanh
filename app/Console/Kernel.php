7d36612c202f6401d57619500f63c7c2ede320b6
nhatduynguyen <15520166@gm.uit.edu.vn>
2021-09-06 23:26:52 +0700

    Create Kernel.php

---
+++ app/Console/Kernel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        'App\Console\Commands\cuisine',
+        'App\Console\Commands\shopping',
+        'App\Console\Commands\entertainment'
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')
+        //          ->hourly();
+        
+        /* $schedule->command('command:cuisine');
+
+        $schedule->command('command:entertainment');
+
+        $schedule->command('command:shopping'); */
+    }
+
+    /**
+     * Register the Closure based commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        require base_path('routes/console.php');
+    }
+}
